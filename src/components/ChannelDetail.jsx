6e87f3b1bb3395774576b5d0c2a6af2e7035be4b
Lucia Moral <lmoralbaena@gmail.com>
2023-08-19 16:55:49 +0200

    immplemented videos and channel

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Videos, ChannelCard } from "./";
 
 const ChannelDetail = () => {
-  return <div>ChannelDetail</div>;
+  return (
+    
+  )
 };
 
 export default ChannelDetail;
